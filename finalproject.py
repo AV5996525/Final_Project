759ea3d987bc398206a9da20d6edb6f7e8b223da
AV599625 <Avaratharajan6525@conestogac.on.ca>
2023-04-05 17:41:27 -0400

    main menu login

--- finalproject.py
+++ finalproject.py
@@ -6,3 +6,36 @@
 #The database will alert the user if a student is failing. A report card may be generated and saved to a text file named 'reportcard.txt'
 #Metric data will also be available in the form of a graph for analysis
 
+def greeting():
+    print("Welcome to the Conestoga Student DataBase, only permited staff members may access this database!")
+    flag1 = False
+    while (flag1 == False):
+        try:
+            logQ = input("Are you ready to log in?")
+            if logQ.isdecimal():
+                raise ValueError
+            elif logQ == "no" or logQ == "No" or logQ == "N" or logQ == "n":
+                exit()
+                break
+            elif logQ == "yes" or logQ == "Yes" or logQ == "Y" or logQ == "y":
+                flag1 = True
+                if flag1 == True:
+                    break
+        except ValueError:
+            print("Please enter Yes or No")
+        finally:
+            flag2 = False
+            logC = input("Enter your log info, if you do not have an account type in 'C' to create a new account ")
+            if logC == 'C' or logC == 'c':
+                userC = {}
+                userN = input("Enter your username:\n")
+                userP = input("Enter your password:\n")
+                userC = {"Username": "{}","Password":"{}".format(userN,userP)}
+    return userN, userP, userC
+
+
+
+greeting()
+
+
+print(userC)

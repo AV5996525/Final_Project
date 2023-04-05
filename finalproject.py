5f104096323a340259c55d5bb92be3cc57ad044b
AV599625 <Avaratharajan6525@conestogac.on.ca>
2023-04-05 17:22:10 -0400

    Intro

--- finalproject.py
+++ finalproject.py
@@ -1,1 +1,8 @@
+#Name:          finalproject.py
+#Author:        AJ Varatharajan
+#Date Created:  April 5, 2023
+#Date Last Modified: April 6, 2023
+#Purpose: This program will store an entire student database. Grades, notes, report cards, progress, academic offenses, and status will be all recorded. 
+#The database will alert the user if a student is failing. A report card may be generated and saved to a text file named 'reportcard.txt'
+#Metric data will also be available in the form of a graph for analysis
 

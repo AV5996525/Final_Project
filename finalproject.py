e2e1803bdd4fa900a118fafafa94b473bf0b1ef7
AV599625 <Avaratharajan6525@conestogac.on.ca>
2023-04-06 18:34:26 -0400

    comments

--- finalproject.py
+++ finalproject.py
@@ -6,11 +6,11 @@
 #A report card may be generated and saved to a text file named 'reportcard.txt'
 #Metric data will also be available in the form of a graph for analysis
 
-import matplotlib.pyplot as plt
-import os
-userCu = {} #intializing empty dictionary
+import matplotlib.pyplot as plt #importing matplotlib.pyplot module 
+import os #importing OS module 
+ #intializing empty dictionary
 student = {} #intializing empty dictionary
-userCp = {} #intializing empty dictionary
+ #intializing empty dictionary
 def greeting(): #login function
     print("Welcome to the Conestoga Student DataBase, only permited staff members may access this database!")
     flag1 = False
@@ -53,15 +53,15 @@ def mainmenu(): #main menu function
     selection = int(input("Pick from one of the following options:\n1. Create New Classroom \n2. Grades \n3. Class Progression Chart \n4. Export Report Card\n5.Enrollment Management\n6. Quit\n"))
     return selection
 #Main Body
-#greeting()
+greeting()
 final = {}
 dict = {}
 dict2 = {}
 flag3 = False
 flag4 = False
 while (flag3==False):
-    mainmenu()#Calling main menu
-    match mainmenu(): #Matching input using switch case selection
+    selection = int(input("Pick from one of the following options:\n1. Create New Classroom \n2. Grades \n3. Class Progression Chart \n4. Export Report Card\n5. Enrollment Management\n6. Quit\n"))
+    match selection: #Matching input using switch case selection
         case 1 :
             print("Student creation - Student database will be subject to overwriting.")
             population = input("Enter the number of students in your classroom:\n")
@@ -74,7 +74,7 @@ while (flag3==False):
             continue    
         case 2 :           
             gradeM = int(input("Enter the student number associated with student you would like to review:\n"))
-            classSel = int(input("Enter the course you wish to modify the grade for student {} {} : \n 1. PROG1783\n 2. INFO1145\n 3. INFO1385\n".format((student[gradeM]['Firstname']),(student[gradeM]['Lastname']))))
+            classSel = int(input("Enter the course you wish to modify the grade for student {} {} : \n 1. PROG1783\n 2. INFO1145\n 3. INFO1385\n".format((student[gradeM]['Firstname']),(student[gradeM]['Lastname'])))) #user input to determine which course will be modified for specific user obtained from accessing dictionary key
             if classSel == 1:
                 gradeEntry = input("Enter the PROG1783 grade value:\n")
                 student[gradeM]['PROG1783'] = int(gradeEntry) 
@@ -91,40 +91,37 @@ while (flag3==False):
                 print(student)       
         case 3 :   
             gradeG = int(input("Enter the student number associated with student you would like to generate a graph for:\n"))
-            left = [1,2,3]
-            height = [student[gradeG]['PROG1783'],student[gradeG]['INFO1145'],student[gradeG]['INFO1385']] 
-            tick_label = ['PROG1783','INFO1145','INFO1385'] 
-            plt.bar(left, height, tick_label = tick_label,width = 0.8, color = ['red', 'green' , 'blue'])
-            plt.xlabel('Course')
-            plt.ylabel('Grade %')
-            plt.title("Progress Report: " + student[gradeG]['Firstname'] + " " + student[gradeG]['Lastname'])
-            plt.show()
+            left = [1,2,3] #creating bar for each course
+            height = [student[gradeG]['PROG1783'],student[gradeG]['INFO1145'],student[gradeG]['INFO1385']] #hieght values obtained from values in dictionary for each course
+            tick_label = ['PROG1783','INFO1145','INFO1385'] #labeling each bar
+            plt.bar(left, height, tick_label = tick_label,width = 0.8, color = ['red', 'green' , 'blue']) #customizing graph for each variable
+            plt.xlabel('Course') #x-axis label
+            plt.ylabel('Grade %')#y-axis label
+            plt.title("Progress Report: " + student[gradeG]['Firstname'] + " " + student[gradeG]['Lastname']) #Graph title
+            plt.show() #calling graph to be displayed
         case 4 :
             confirmR = input("Are you sure you want to export a report card for all students?\n")
             if confirmR == 'Y':
-             newFile = open("reportcard.txt", 'w')    
+             newFile = open("reportcard.txt", 'w')    #creating 'reportcard.txt' file for writing 
              
             newFile.write("Student List: ")
                 
             newFile.write('\n')
-            for x,p in (student.items()):
+            for x,p in (student.items()): #looping through dictionary
                 newFile.write("Student # ")
-                newFile.write("{}\t{}\n".format(str(x),str(p)))
-                    #newFile.write(str(g))
-                    #newFile.write("{}\n".format(str(x)))
-                    #newFile.write("-------------------------------\n")
-            newFile.close()
+                newFile.write("{}\t{}\n".format(str(x),str(p))) #writing each dictionary item to 'reportcard.txt' file
+            newFile.close() #closing file as per good I/O practice
             print("The summary txt file has been succesfully written to ", os.getcwd()) #Displaying file pathway
-            print("File name: ", newFile) 
+            print("File name: ", newFile) #displaying file name
 
         case 5 :
             print("Enrollment management:")
             choice = int(input("1. Delete Student\n2. Add Student\n3. Exit to main menu\n:"))
             if choice == 1:
                 searchD = int(input("Enter the student number associated with student you would like to delete:\n"))
-                confirmD = input("Are you sure you want to delete {} {}".format(student[searchD]['Firstname'],student[searchD]['Lastname']))
+                confirmD = input("Are you sure you want to delete {} {}".format(student[searchD]['Firstname'],student[searchD]['Lastname'])) #confirmation to delete, reminding user with specified user name to be deleted.
                 if confirmD == 'Y':
-                    del student[searchD]
+                    del student[searchD] #utilizing dictionary key to delete entry, obtained from user input
                     mainmenu()
                 elif confirmD == 'N':
                     pass
@@ -132,7 +129,7 @@ while (flag3==False):
                 student[int(len(student))+1] = {}
                 firstnameC = input("Enter first name:")
                 lastnameC = input("Enter last name")
-                student[int(len(student))+1] = {"Firstname": firstnameC, "Lastname":lastnameC}
+                student[int(len(student))+1] = {"Firstname": firstnameC, "Lastname":lastnameC}#writing each dictionary value
                 print(student.items())
                 mainmenu()
             elif choice == 3:
